993e889221512b2855d31327743d6ed01587f7ef
aeilmors <zhuiyitaosheng@gmail.com>
2014-10-21 19:42:41 +0800

    4.11.3

---
+++ 4_11_3.cpp
@@ -0,0 +1,31 @@
+/***************************************
+ Author : jt
+ Mail : zhuiyitaosheng@gmail.com
+***************************************/
+
+#include <iostream>
+#include <cstring>
+//#include <vector>
+//#include <cstddef>
+
+using namespace::std;
+
+int main()
+{
+	int i;
+	double d;
+	const string *ps;
+	char *pc;
+	void *pv;
+
+	// pv = (void *)ps;
+	pv = static_cast<void *>(const_cast<string *>(ps));
+	// i = int(*pc);
+	i = static_cast<int>(*pc);
+	// pv = &d;
+	pv = static_cast<void *>(&d);
+	// pc = (char *)pv;
+	pc = static_cast<char *>(pv);
+
+	return 0;
+} 
