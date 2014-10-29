80139c87a422afedf72abc4ada2f535ce059d54f
aeilmors <zhuiyitaosheng@gmail.com>
2014-10-29 23:20:15 +0800

    BiTree

---
+++ Bi_Tree.cpp
@@ -0,0 +1,29 @@
+/***************************************
+ Author : jt
+ Mail : zhuiyitaosheng@gmail.com
+***************************************/
+
+#include <iostream>
+//#include <string>
+//#include <vector>
+//#include <cstddef>
+//#include <exception>
+//#include <stdexcept>
+//#include <new>
+//#include <type_info>
+
+using namespace::std;
+
+using Elemtype = int;
+
+ typedef struct BiTreeNode {
+ 	Elemtype data;
+ 	struct BiTreeNode *lchild, *rchild;
+ }BiTreeNode, *BiTree;
+
+int main()
+{
+
+
+	return 0;
+}
