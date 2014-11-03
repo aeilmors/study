3be560e0747af10ebf10c39023ed403af42f4183
aeilmors <zhuiyitaosheng@gmail.com>
2014-11-03 12:00:58 +0800

    class Person

---
+++ workspace/PersonApplication/Person.cpp
@@ -0,0 +1,38 @@
+/*
+ * Person.cpp
+ *
+ *  Created on: 2014年11月2日
+ *      Author: jt
+ */
+
+#include "Person.h"
+#include <string>
+
+Person::Person(std::istream &is) {
+	// TODO Auto-generated constructor stub
+	read(is, *this);
+}
+
+Person::~Person() {
+	// TODO Auto-generated destructor stub
+}
+
+const std::string &Person::getAddress() const {
+	return address;
+}
+
+const std::string &Person::getName() const {
+	return name;
+}
+
+std::istream &read(std::istream &is, Person &person) {
+	is >> person.name >> person.address;
+
+	return is;
+}
+
+std::ostream &print(std::ostream &os, const Person &person) {
+	os << person.name << person.address;
+
+	return os;
+}
---
+++ workspace/PersonApplication/Person.h
@@ -0,0 +1,32 @@
+/*
+ * Person.h
+ *
+ *  Created on: 2014年11月2日
+ *      Author: jt
+ */
+
+#ifndef PERSON_H_
+#define PERSON_H_
+
+#include <iostream>
+#include <string>
+
+class Person {
+public:
+	Person() = default;
+	Person(const std::string &name, const std::string &address) : name(name), address(address) {}
+	Person (std::istream &);
+	virtual ~Person();
+
+	const std::string &getName() const;
+	const std::string &getAddress() const;
+
+//private:
+	std::string name;
+	std::string address;
+};
+
+std::istream &read(std::istream &, Person &);
+std::ostream &print(std::ostream &, const Person &);
+
+#endif /* PERSON_H_ */
